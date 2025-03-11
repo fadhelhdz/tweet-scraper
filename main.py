53300a9eec29c44edee52b48b942de4c1ed85339
fadhelhdz <hariz.dz97@gmail.com>
2025-03-11 22:17:47 +0900

    base main

--- main.py
+++ main.py
@@ -6,8 +6,23 @@ from configparser import ConfigParser
 from random import randint
 import asyncio
 
-
 client = Client(language='en-US')
+MINIMUM_TWEETS = 100
+QUERY = "100 hari prabowo"
+
+async def get_tweets(tweets):
+    if tweets is None:
+        #* get tweets
+        print(f'{datetime.now()} - Getting tweets...')
+        tweets = await client.search_tweet(QUERY, product='Top')
+    else:
+        wait_time = randint(5, 10)
+        print(f'{datetime.now()} - Getting next tweets after {wait_time} seconds ...')
+        time.sleep(wait_time)
+        tweets = await tweets.next()
+
+    return tweets
+
 async def main():
     # Load credentials
     config = ConfigParser()
@@ -22,6 +37,37 @@ async def main():
 
     # Load cookies instead of login again
     client.load_cookies('cookies.json')
+
+    tweet_count = 0
+    tweets = None
+
+    while tweet_count < MINIMUM_TWEETS:
+
+        try:
+            tweets = await get_tweets(tweets)
+        except TooManyRequests as e:
+            rate_limit_reset = datetime.fromtimestamp(e.rate_limit_reset)
+            print(f'{datetime.now()} - Rate limit reached. Waiting until {rate_limit_reset}')
+            wait_time = rate_limit_reset - datetime.now()
+            time.sleep(wait_time.total_seconds())
+            continue
+
+        if not tweets:
+            print(f'{datetime.now()} - No more tweets found')
+            break
+
+        for tweet in tweets:
+            tweet_count += 1
+            tweet_data = [tweet_count, tweet.user.name, tweet.text, tweet.created_at, tweet.retweet_count, tweet.favorite_count]
+            
+            with open('tweets.csv', 'a', newline='', encoding='utf-8') as file:
+                writer = csv.writer(file)
+                writer.writerow(tweet_data)
+
+        print(f'{datetime.now()} - Got {tweet_count} tweets')
+
+
+    print(f'{datetime.now()} - Done! Got {tweet_count} tweets found')
 
 if __name__ == "__main__":
     asyncio.run(main())
